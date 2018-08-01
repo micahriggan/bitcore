333f365f1d36fce902a55bfd7420de94b798fc63
Micah Riggan <mriggan@bitpay.com>
2018-08-01 15:41:13 -0400

    Breaking out into function

--- packages/bitcore-node/src/providers/transforms.ts
+++ packages/bitcore-node/src/providers/transforms.ts
@@ -9,102 +9,103 @@ export class ListTransactionsStream extends Transform {
   constructor(private wallet: MongoBound<IWallet>) {
     super({
       objectMode: true,
-      highWaterMark: 500,
-      flush: cb => {
-        Promise.all(this.allWritten).then(() => cb());
-      }
     });
   }
 
-  async _transform(tx, _, done) {
-    const writeToStream = async transaction => {
-      const self = this;
-      const [inputs, outputs] = await Promise.all([
-        CoinModel.collection
-          .find({
-            chain: transaction.chain,
-            network: transaction.network,
-            spentTxid: transaction.txid
-          })
-          .addCursorFlag('noCursorTimeout', true)
-          .toArray(),
-        CoinModel.collection
-          .find({
-            chain: transaction.chain,
-            network: transaction.network,
-            mintTxid: transaction.txid
-          })
-          .addCursorFlag('noCursorTimeout', true)
-          .toArray()
-      ]);
+  async writeTxToStream(transaction) {
+    const [inputs, outputs] = await Promise.all([
+      CoinModel.collection
+        .find({
+          chain: transaction.chain,
+          network: transaction.network,
+          spentTxid: transaction.txid
+        })
+        .addCursorFlag('noCursorTimeout', true)
+        .toArray(),
+      CoinModel.collection
+        .find({
+          chain: transaction.chain,
+          network: transaction.network,
+          mintTxid: transaction.txid
+        })
+        .addCursorFlag('noCursorTimeout', true)
+        .toArray()
+    ]);
 
-      transaction.inputs = inputs;
-      transaction.outputs = outputs;
-      const wallet = this.wallet._id!.toString();
-      const sending = transaction.inputs.some(input => {
-        let contains = false;
-        for (let inputWallet of input.wallets) {
-          if (inputWallet.equals(wallet)) {
-            contains = true;
-          }
-        }
-        return contains;
-      });
-
-      const totalInputs = transaction.inputs.reduce((total, input) => {
-        return total + input.value;
-      }, 0);
-      let totalOutputs = 0;
-
-      for (let output of transaction.outputs) {
-        totalOutputs += output.value;
-        if (sending) {
-          self.push(
-            JSON.stringify({
-              txid: transaction.txid,
-              category: 'send',
-              satoshis: -output.value,
-              height: transaction.blockHeight,
-              address: output.address,
-              outputIndex: output.vout,
-              blockTime: transaction.blockTimeNormalized
-            }) + '\n'
-          );
-        }
-        let receiving = false;
-        for (let outputWallet of output.wallets) {
-          if (outputWallet.equals(wallet)) {
-            receiving = true;
-          }
-        }
-        if (receiving) {
-          self.push(
-            JSON.stringify({
-              txid: transaction.txid,
-              category: 'receive',
-              satoshis: output.value,
-              height: transaction.blockHeight,
-              address: output.address,
-              outputIndex: output.vout,
-              blockTime: transaction.blockTimeNormalized
-            }) + '\n'
-          );
+    transaction.inputs = inputs;
+    transaction.outputs = outputs;
+    const wallet = this.wallet._id!.toString();
+    const sending = transaction.inputs.some(input => {
+      let contains = false;
+      for (let inputWallet of input.wallets) {
+        if (inputWallet.equals(wallet)) {
+          contains = true;
         }
       }
-      const fee = totalInputs - totalOutputs;
-      if (sending && fee > 0) {
-        self.push(
+      return contains;
+    });
+
+    const totalInputs = transaction.inputs.reduce((total, input) => {
+      return total + input.value;
+    }, 0);
+    let totalOutputs = 0;
+
+    for (let output of transaction.outputs) {
+      totalOutputs += output.value;
+      if (sending) {
+        this.push(
           JSON.stringify({
             txid: transaction.txid,
-            category: 'fee',
-            satoshis: -fee,
+            category: 'send',
+            satoshis: -output.value,
             height: transaction.blockHeight,
+            address: output.address,
+            outputIndex: output.vout,
             blockTime: transaction.blockTimeNormalized
           }) + '\n'
         );
       }
-    };
-    this.allWritten.push(writeToStream(tx));
+      let receiving = false;
+      for (let outputWallet of output.wallets) {
+        if (outputWallet.equals(wallet)) {
+          receiving = true;
+        }
+      }
+      if (receiving) {
+        this.push(
+          JSON.stringify({
+            txid: transaction.txid,
+            category: 'receive',
+            satoshis: output.value,
+            height: transaction.blockHeight,
+            address: output.address,
+            outputIndex: output.vout,
+            blockTime: transaction.blockTimeNormalized
+          }) + '\n'
+        );
+      }
+    }
+    const fee = totalInputs - totalOutputs;
+    if (sending && fee > 0) {
+      this.push(
+        JSON.stringify({
+          txid: transaction.txid,
+          category: 'fee',
+          satoshis: -fee,
+          height: transaction.blockHeight,
+          blockTime: transaction.blockTimeNormalized
+        }) + '\n'
+      );
+    }
+  }
+
+  async _transform(tx, _, done) {
+    this.allWritten.push(this.writeTxToStream(tx));
     done();
+  }
+
+  async _flush(cb) {
+    await Promise.all(this.allWritten);
+    cb();
   }
 }
